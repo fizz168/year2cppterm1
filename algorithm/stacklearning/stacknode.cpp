f9c6982f6944df0d59bee69c74ce9b67275fa85b
fizz168 <nheanomra@gamil.com>
2025-11-22 13:27:14 +0700

    feat: homework week 6 form

--- algorithm/stacklearning/stacknode.cpp
+++ algorithm/stacklearning/stacknode.cpp
@@ -16,12 +16,34 @@ public:
 Stack(){
     top = nullptr;
     size = 0;
+
 }
 void push(int data){
     Node* newNode = new Node(data);
     newNode->next = top;
     top = newNode;
     size++;
+}
+void pop(){
+    if(top == 0){
+        cout<<"stack is empty"<<endl;
+        return;
+    }
+    Node* temp = top;
+    top = top->next;
+    temp->next = nullptr;
+    delete temp;
+    size--;
+}
+bool isempty(){
+     return (top == nullptr);
+}
+float peek(){
+ if(top == 0){
+    cout<<"stack is empty";
+ }
+    return top->data;
+ 
 }
 void print(){
     Node* temp = top;
@@ -35,5 +57,7 @@ int main(){
     Stack s;
     s.push(5);
     s.push(4);
+    s.push(6);
+    s.peek();
     s.print();
 }
