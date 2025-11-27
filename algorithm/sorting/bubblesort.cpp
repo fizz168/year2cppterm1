e071cc0d619fa0e75c642ff8c12f7ed7b0c65da5
fizz168 <nheanomra@gmail.com>
2025-11-27 17:26:42 +0700

    sorting

---
+++ algorithm/sorting/bubblesort.cpp
@@ -0,0 +1,28 @@
+// no one use this shit lmao 
+#include<iostream>
+using namespace std;
+void bubblesort(int arr[], int n){ // time complexity is o(n^2)
+    for(int i = 0; i < n - 1; i++){
+        for(int j = n; j < n - 1 - i; j++){
+            if(arr[j] > arr[j+1]){// handle the swap
+                swap(arr[j], arr[j+1]); // handle the swap
+            }
+        }
+
+    }
+}
+void printArr(int arr[], int n){
+    for(int i = 0; i < n ; i++){
+        cout<<arr[i]<< " "<<endl;
+
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[] = {1, 3, 4, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubblesort(arr, n);
+    printArr(arr, n);
+
+    return 0;
+}
